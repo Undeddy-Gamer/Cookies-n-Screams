0cb33367564ada2df7df79cd3fa5a80ef8f7af6e
GlitcherOG <54088472+GlitcherOG@users.noreply.github.com>
2019-08-16 13:38:17 +1000

    Merged Both Project Versions
    A basic merge of the projects.

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool isPaused = false;
+    public GameObject pauseMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+    public void TogglePause()
+    {
+        if (isPaused)
+        {
+            Time.timeScale = 1;
+            pauseMenu.SetActive(false);
+            isPaused = false;
+            return;
+        }
+        else
+        {
+            Time.timeScale = 0;
+            pauseMenu.SetActive(true);
+            isPaused = true;
+            return;
+        }
+    }
+}
