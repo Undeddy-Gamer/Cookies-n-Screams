878b2eedb60b06d8f87c1e4519a18fb3d4574a33
GlitcherOG <54088472+GlitcherOG@users.noreply.github.com>
2019-08-22 11:14:58 +1000

    Added credits and limited the auto clicker force

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,55 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Player : MonoBehaviour    
+public class Player : MonoBehaviour
 {
 
     //<access-specifier> <data-type> <variable name>
-    public Rigidbody2D PlayerRigid;
-    public float acceleration = 5F;
-    public float maxSpeed;
-    public float currentAutoClickForce;
-    private Vector3 direction;
-    public Animator Frog; 
-    public Animator Human;
-    public static float currentSpeed;
-    bool Test;
+    public Rigidbody2D PlayerRigid; // Rigidboy for the Player and monster
+    public float acceleration = 5F; //
+    public float maxSpeed = 650; //Max autoclick speed
+    public float currentAutoClickForce; //Adjust the ammout of force for debugging 
+    private Vector2 direction; //Vector2 for the rigidbody movement
+    public Animator Frog; //The animator for the monster
+    public Animator Human; //The animator for the human
+    public static float currentSpeed; 
+    bool testMovement;
+    public Slider screamSlider; //Slider refrence for the screams perSecondBar
 
     private void Start()
     {
+        //On Start begin repeating the void AddAutoForce
         InvokeRepeating("AddAutoForce", 1, 0.1f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Frog.speed = (PlayerRigid.velocity.x / maxSpeed) *12;
-        Human.speed = (PlayerRigid.velocity.x / maxSpeed) *12;
-        //Debug.Log(PlayerRigid.velocity.x);
-        if (PlayerRigid.velocity.magnitude < 0.5f && !Test)
+        Frog.speed = (PlayerRigid.velocity.x / maxSpeed) * 12;
+        Human.speed = (PlayerRigid.velocity.x / maxSpeed) * 13;
+        if (PlayerRigid.velocity.magnitude < 0.5f && !testMovement)
         {
             gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX;
-            Test = true;
+            testMovement = true;
         }
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{
-        //    AddMonsterForce(acceleration);
-        //}
+        screamSlider.value = Click.perSecondValue / maxSpeed;
     }
 
     void AddAutoForce()
     {
-        AddMonsterForce((currentAutoClickForce * 0.2f) * Click.perSecondValue);
-        
-        //(float)currentAutoClickForce / 100)
+        if (Click.perSecondValue <= maxSpeed)
+        {
+            AddMonsterForce((currentAutoClickForce * 0.2f) * Click.perSecondValue);
+        }
+        else
+        {
+            AddMonsterForce((currentAutoClickForce * 0.2f) * maxSpeed);
+        }
     }
-
     public void AddMonsterForce(float amount = 5f)
     {
         if (PlayerRigid.velocity.magnitude > 0.2f)
         {
-            Test = false;
+            testMovement = false;
         }
         gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
         direction = new Vector3(1 * amount, 0, 0);
